ae5949c015504f1ac463600b389c25b5948e6d30
Seymour Teets <claytonlaneteets@gmail.com>
2015-05-18 15:16:13 -0700

    Signed-off-by: Seymour Teets <claytonlaneteets@gmail.com>

--- Hmwk/Assignment_6/Salsa/main.cpp
+++ Hmwk/Assignment_6/Salsa/main.cpp
@@ -22,17 +22,37 @@ int main(int argc, char** argv) {
     int SIZE = 5;
     string sName[SIZE] = {"mild","medium","sweet","hot","zesty"};
     int sSold[SIZE] = {};
+    int high, h;
+    int low, l;
     //Prompt for input
-    cout<<"Enter sales for all 5 salsa types"<<endl;
+    cout<<"Enter sales for all 5 salsa types."<<endl;
     for (int i=0; i<SIZE; i++){
         cout<<setw(7)<<sName[i]<<" = ";
         cin>>sSold[i];
     }
     cout<<endl;
     //Output Sales
+    cout<<"Salsa Sales:"<<endl;
     for (int i=0; i<SIZE; i++){
         cout<<setw(7)<<sName[i]<<" = "<<sSold[i]<<endl;
     }
+    cout<<endl;
+    //Find highest and lowest sales
+    high = sSold[0];
+    low = sSold[0];
+    for (int i=0; i<SIZE; i++){
+        if (sSold[i] > high) {
+            high = sSold[i];
+            h=i;
+        }
+        if (sSold[i] < low) {
+            low = sSold[i];
+            l=i;
+        }
+    }
+    //Output highest and lowest sales
+    cout<<"The highest sales are "<<sName[h]<<" salsa, at "<<sSold[h]<<" sales."<<endl;
+    cout<<"The lowest sales are "<<sName[l]<<" salsa, at "<<sSold[l]<<" sales."<<endl;
     //Executio Disicere
     return 0;
 }
