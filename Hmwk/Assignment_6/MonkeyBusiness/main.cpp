961d1d7e8f50037c7f0c98fb17f88047e60e3b93
Seymour Teets <claytonlaneteets@gmail.com>
2015-05-19 14:11:35 -0700

    Signed-off-by: Seymour Teets <claytonlaneteets@gmail.com>

--- Hmwk/Assignment_6/MonkeyBusiness/main.cpp
+++ Hmwk/Assignment_6/MonkeyBusiness/main.cpp
@@ -22,12 +22,14 @@ int main(int argc, char** argv) {
     string weekday[7] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
     string mName[3] = {"Jimbo","Kong","Ceaser"};
     int monkey[3][7] = {};
-    int average;
+    float average;
+    int high, hName, hFood;
+    int low, lName, lFood;
     //Collect food eaten for the week
     for (int i=0; i<3; i++){
-        cout<<mName[i]<<":"<<endl;
+        cout<<mName[i]<<endl;
         for (int u=0; u<7; u++){
-            cout<<weekday[u]<<":";
+            cout<<setw(9)<<weekday[u]<<" : ";
             cin>>monkey[i][u];
         }
         cout<<endl;
@@ -39,8 +41,32 @@ int main(int argc, char** argv) {
         }
     }
     average /= 7;
-    //Find most food eaten
-    
+    //Find most and least food eaten
+    high = monkey[0][0];
+    hName = 0;
+    hFood = 0;
+    low = monkey[0][0];
+    lName = 0;
+    lFood = 0;
+    for (int i=0; i<3; i++){
+        for (int u=0; u<7; u++){
+            if (monkey[i][u]>high){
+                high = monkey[i][u];
+                hName = i;
+                hFood = u;
+            }
+            if (monkey[i][u]<low){
+                low = monkey[i][u];
+                lName = i;
+                lFood = u;
+            }
+        }
+    }
+    //Output average, high, and low
+    cout<<mName[hName]<<" ate the most food ("<<monkey[hName][hFood]<<" lbs) on "<<weekday[hFood]<<"."<<endl;
+    cout<<mName[lName]<<" ate the least food ("<<monkey[lName][lFood]<<" lbs) on "<<weekday[lFood]<<"."<<endl;
+    cout<<fixed<<showpoint<<setprecision(2);
+    cout<<"The monkey family ate an average of "<<average<<" lbs of food per day."<<endl;
     //Executio Disicere
     return 0;
 }
